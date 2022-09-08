8ab52051f227520a9f5d9c2d913ba5548de25a84
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-08 17:49:28 +0530

    Sorting fruits

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
 
-let stageEl = document.getElementById("stage")
-let fightButton = document.getElementById("fightButton")
 
-fightButton.addEventListener("click", function() {
-    // Challenge:
-    // When the user clicks on the "Pick Fighters" button, pick two random
-    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
-    let randomFighter1 = Math.floor(Math.random() * fighters.length)
-    let randomFighter2 = Math.floor(Math.random() * fighters.length)
-    let fighterA = fighters[randomFighter1]
-    let fighterB = fighters[randomFighter2]
-    stageEl.textContent = fighterA + " vs " + fighterB
+// Create a function that puts the apples onto the appleShelf
+// and the oranges onto the orangeShelf. Use a for loop,
+// a conditional statement, and the textContent property.
+function fruitShelf() {
 
-})
+  for (let i = 0; i < fruit.length; i++) {
 
-// console.log(fighters.length)
-
+     if ( fruit[i] === "🍎") {
+         appleShelf.textContent += fruit[i]
+    } else if (fruit[i] === "🍊") {
+        orangeShelf.textContent += fruit[i]
+    }
+   }
+}
+fruitShelf()
