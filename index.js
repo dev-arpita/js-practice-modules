373765167f3c23aa402a990f6cc9b5081c0ab4ff
Arpita Roy <arpita.roy.workar@gmail.com>
2022-12-29 19:45:32 +0530

    Update changes

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let fruits = document.getElementById("fruit-shelf")
+fruits.textContent = fruit.join("")
 let appleShelf = document.getElementById("apple-shelf")
 let orangeShelf = document.getElementById("orange-shelf")
 
-
+document.getElementById("srotButton").addEventListener("click", fruitShelf)
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
 function fruitShelf() {
+        return fruit.map(fruit => {
+        return( fruit === "🍎") ? appleShelf.textContent += fruit : orangeShelf.textContent += fruit
+    })
 
-  for (let i = 0; i < fruit.length; i++) {
-
-     if ( fruit[i] === "🍎") {
-         appleShelf.textContent += fruit[i]
-    } else if (fruit[i] === "🍊") {
-        orangeShelf.textContent += fruit[i]
-    }
-   }
 }
-fruitShelf()
+// fruitShelf()
